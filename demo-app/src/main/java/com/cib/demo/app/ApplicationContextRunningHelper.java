96296aa6114b419f1782db2c1cc6cb9ba1381e2e
zhengyigitee <qq1339967840@gmail.com>
2024-09-20 15:05:08 +0800

    框架基本搭建

---
+++ demo-app/src/main/java/com/cib/demo/app/ApplicationContextRunningHelper.java
@@ -0,0 +1,15 @@
+package com.cib.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class ApplicationContextRunningHelper {
+
+    public static void beforeRunningStep(SpringApplication springApplication, String[] args) {
+    }
+
+    public static void afterRunningStep(ConfigurableApplicationContext context, SpringApplication springApplication, String[] args) {
+
+    }
+
+}
--- demo-app/src/main/java/com/cib/demo/app/DemoApplication.java
+++ demo-app/src/main/java/com/cib/demo/app/DemoApplication.java
@@ -1,4 +1,42 @@
 package com.cib.demo.app;
 
+
+import com.google.common.base.Stopwatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
 public class DemoApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+    public static void main(String[] args) {
+
+        Stopwatch stopWatch = Stopwatch.createStarted();
+
+        SpringApplication springApplication = new SpringApplication(DemoApplication.class);
+
+        ApplicationContextRunningHelper.beforeRunningStep(springApplication, args);
+
+        ConfigurableApplicationContext context = springApplication.run(args);
+
+        ApplicationContextRunningHelper.afterRunningStep(context, springApplication, args);
+
+        callback(context, stopWatch);
+    }
+
+
+    private static void callback(ConfigurableApplicationContext context, Stopwatch stopWatch) {
+        stopWatch.stop();
+        String name = context.getEnvironment().getProperty("spring.application.name");
+        String port = context.getEnvironment().getProperty("server.port");
+        logger.info("======服务{}:{}启动成功，本次启动累计耗时{}毫秒======", name, port, stopWatch.elapsed(TimeUnit.MILLISECONDS));
+    }
+
+
 }
