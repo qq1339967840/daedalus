8670f3b09f3cb9b15f0c056762198cb442f90f62
zhengyigitee <qq1339967840@gmail.com>
2024-10-22 15:20:27 +0800

    编写加密工具

--- icarus-core/src/main/java/com/cib/icarus/core/Icarus.java
+++ icarus-core/src/main/java/com/cib/icarus/core/Icarus.java
@@ -3,4 +3,7 @@ package com.cib.icarus.core;
 public class Icarus {
     public final static String VERSION = "Icarus-" + Icarus.class.getPackage().getImplementationVersion();
 
+    public static String getVersion() {
+        return VERSION;
+    }
 }
